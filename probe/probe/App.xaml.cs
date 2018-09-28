fcb60aa762402e322dad17bfada184882771e389
Ronald Volkmann <rvolkmann@h3ko.de>
2018-09-28 10:28:51 +0200

    first demo

--- probe/probe/App.xaml.cs
+++ probe/probe/App.xaml.cs
@@ -3,7 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-namespace probe
+namespace Phoneword
 {
     public partial class App : Application
     {
